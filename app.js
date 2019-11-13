05023e8035a36744bd4bbe7fcaf435b07c37f1bf
ulucay <clarkulucay@gmail.com>
2019-11-13 14:31:40 -0600

    API request is completed

---
+++ app.js
@@ -0,0 +1,31 @@
+let request = new XMLHttpRequest();
+
+let city = 'Bursa';
+
+request.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=2d8da3c8a917b7f3c177458dd82c4d27`, true);
+
+request.onload = function() {
+    let data = JSON.parse(this.response);
+
+    if(request.status >= 200 && request.status < 400) {
+        
+        let city = data.name;
+        let main = data.main.temp;
+    
+        let tempSpan = document.querySelector("#temp");
+        let citySpan = document.querySelector("#city");
+
+        citySpan.textContent = city;
+        tempSpan.textContent = main;
+
+        console.log(city);
+        console.log(main);
+  
+    } 
+    else
+    {
+        console.log('error')
+    }
+};
+
+request.send();

1f7ae79675440ebeef0e093b4d438c694e780859
ulucay <clarkulucay@gmail.com>
2019-11-14 09:17:55 -0600

    card layout implemented

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let request = new XMLHttpRequest();
 const APIKEY = '2d8da3c8a917b7f3c177458dd82c4d27';
-let city = 'Bursa';
+let city = 'Austin';
 
 
 request.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKEY}`, true);
@@ -11,13 +11,21 @@ request.onload = function() {
     if(request.status >= 200 && request.status < 400) {
         
         let city = data.name;
-        let main = data.main.temp;
+        let main = Math.trunc(data.main.temp);
+        let wind = data.wind.speed;
+        let humidity = data.main.humidity + "%";
+
     
         let tempSpan = document.querySelector("#temp");
         let citySpan = document.querySelector("#city");
+        let windSpan = document.querySelector("#wind");
+        let humiditySpan = document.querySelector("#humidity");
 
         citySpan.textContent = city;
         tempSpan.textContent = main;
+        windSpan.textContent = wind;
+        humiditySpan.textContent = humidity;
+
 
         console.log(city);
         console.log(main);
